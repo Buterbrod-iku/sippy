9810d57c839450e977d8fb306ca8f74940e57708
KirillXL <110365222+KirillXL@users.noreply.github.com>
2024-05-19 15:44:41 +0700

    Changes 2

--- src/components/map/L_Icons.jsx
+++ src/components/map/L_Icons.jsx
@@ -1,4 +1,10 @@
 import { Icon } from 'leaflet';
+import pub_img from '@/../public/ico/sippy.svg'
+import house_img from '@/../public/ico/house.svg'
+import party_img from '@/../public/ico/party.svg'
+import film_img from '@/../public/ico/film.svg'
+import sport_img from '@/../public/ico/sport.svg'
+import add_img from '@/../public/ico/add.svg'
 
 export default class LeafletCustomIcons {
 
@@ -6,14 +12,39 @@ export default class LeafletCustomIcons {
         return await axios.post(`${HOST}/point/nearest`, object)
     }
 
-//     static pub_icon = L.icon({
-//         // iconRetinaUrl: require('@/../public/image/developers/Dmitry.jpg'),
-//         iconUrl: require('@/../public/ico/happy.png')
-//         //shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-//    }) 
     static pub_icon = new Icon({
-        iconUrl: require('@/../public/ico/happy.png'),
-        iconSize: [44, 44]
+        iconUrl: pub_img.src,
+        iconSize: [32, 32]
     });
+
+    static house_icon = new Icon({
+        iconUrl: pub_img.src,
+        iconSize: [32, 32]
+    });
+
+    static party_icon = new Icon({
+        iconUrl: party_img.src,
+        iconSize: [32, 32]
+    });
+
+    static film_icon = new Icon({
+        iconUrl: film_img.src,
+        iconSize: [32, 32]
+    });
+
+    static sport_icon = new Icon({
+        iconUrl: sport_img.src,
+        iconSize: [32, 32]
+    });
+
+    static add_icon = new Icon({
+        iconUrl: add_img.src,
+        iconSize: [32, 32]
+    });
+
+// Все что добавлено тут нужно добавить и в map.jsx (iconPairs)
+
+   
+
 
 }
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,11 +1,12 @@
 "use client"
 import style from './map.module.scss'
 
-import { MapContainer, TileLayer, CircleMarker, Popup, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, CircleMarker, Popup, Marker, ZoomControl } from 'react-leaflet';
 import { useMapEvents } from 'react-leaflet/hooks'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import L_Icons from "./L_Icons";
+import CustomPopup from '../customPopup/customPopup';
 
 import 'leaflet/dist/leaflet.css'
 
@@ -25,33 +26,35 @@ const Map = ({points, center}) => {
 
     return (
         <div>
-            <MapContainer center={[center.long, center.lat]} zoom={11}>
+            <MapContainer center={[center.long, center.lat]} zoom={13} zoomControl={false}>
 
                 <TileLayer
-                    attribution='&copy'
+                    attribution=''
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 {
-                    // В КОМПОНЕНТУ
+
                     points.map(point => 
-                        <CustomIconMarker key={point.id} pos={[point.longitude, point.latitude]} _color="blue" popup_content={point.category_id}/>
-                            
+                        <CustomIconMarker key={point.id} pos={[point.longitude, point.latitude]} _color="blue" popup_content={point.category_id} category={point.category_id}/>     
                     )
                 }
                 {
-                    // В КОМПОНЕНТУ X2 (ЭТО ГЕО)
+
                     (locationPoint.latitude == 0 && locationPoint.longitude == 0) ? "" : 
                     <CustomCircleMarker pos={[locationPoint.latitude, locationPoint.longitude]} _color="red" popup_content="GEO"/>
                     
                 }
                     
                 {
-                    // В КОМПОНЕНТУ X3 (ЭТО ПО КЛИКУ)
+
                     (clickPoint.latitude == 0 && clickPoint.longitude == 0) ? "" : 
-                    <CustomCircleMarker pos={[locationPoint.latitude, locationPoint.longitude]} _color="red" popup_content="NEW"/>
+                    <CustomIconMarker pos={[clickPoint.latitude, clickPoint.longitude]} popup_content="Добавление" category={"add"}/>
                 }
+
                 
+                
+                <ZoomControl position="bottomleft"/>
                 <MapClickListen setLoc={setLocationPoint} setClick={setClickPoint}/>
             </MapContainer>
 
@@ -62,13 +65,21 @@ const Map = ({points, center}) => {
 
 export default Map;
 
-const CustomIconMarker = ({pos, _color, popup_content}) => {
+const CustomIconMarker = ({pos, _color, popup_content, category}) => {
+    const iconPairs = {
+        "pubs": L_Icons.pub_icon,
+        "at_home": L_Icons.house_icon,
+        "sabantui": L_Icons.party_icon,
+        "cinema": L_Icons.film_icon,
+        "sport": L_Icons.sport_icon,
+        "add": L_Icons.add_icon
+    }
+    
+    
     return (
-    <Marker icon={L_Icons.pub_icon} position={pos} radius={10} color="transparent" fillColor={_color} opacity={1}>
-        <Popup className={style.popup}>
-            <div className={style.popupDiv}>
-                {popup_content}
-            </div>
+    <Marker icon={iconPairs[category]} position={pos} radius={10} color="transparent" fillColor={_color} opacity={1}>
+        <Popup>
+            <CustomPopup/>
         </Popup>
     </Marker>
     )
